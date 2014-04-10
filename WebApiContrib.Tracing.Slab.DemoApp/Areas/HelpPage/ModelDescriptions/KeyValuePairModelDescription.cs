034c04c732c754802a77419a0750e84c34097677
damienbod <damien_bod@hotmail.com>
2014-04-10 14:25:22 +0200

    Creating a demo App and using NuGet package

---
+++ WebApiContrib.Tracing.Slab.DemoApp/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace WebApiContrib.Tracing.Slab.DemoApp.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ WebApiContrib.Tracing.Slab.DemoApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiContrib.Tracing.Slab.DemoApp.WebApiApplication" Language="C#" %>
